21901425ac2db197d9333dc86d8a2f79c56787b8
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-04-26 11:54:21 +0200

    Added __FILE__ __LINE__ and __func__ in error message

--- eco_perf/cute_terminal/definitions/error.h
+++ eco_perf/cute_terminal/definitions/error.h
@@ -18,19 +18,32 @@ const char *get_ct_error_str(ct_error_t error_code);
 
 #ifdef CT_ECO_MODE
 #define CT_ERROR(error_code, message, ...)
+#define CT_ASSERT(condition, error_code, message, ...)
 #else
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define CT_ERROR(error_code, message, ...)                   \
     {                                                        \
         char _ct_error_str_buffer[CT_ERROR_STR_BUFFER_SIZE]; \
         sprintf(                                             \
             _ct_error_str_buffer,                            \
-            "%s : " message "\n",                            \
+            "In function %s (l.%d %s)\n%s : " message "\n",  \
+            __func__,                                        \
+            __LINE__,                                        \
+            __FILE__,                                        \
             get_ct_error_str(error_code),                    \
             ##__VA_ARGS__);                                  \
         fprintf(stderr, "%s", _ct_error_str_buffer);         \
         exit(1);                                             \
+    }
+
+#define CT_ASSERT(condition, error_code, message, ...)   \
+    {                                                    \
+        if (!(condition))                                \
+        {                                                \
+            CT_ERROR(error_code, message, ##__VA_ARGS__) \
+        }                                                \
     }
 #endif
 
