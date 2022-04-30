c0634bd055b8d2405910740b69de958eed012bf2
GregoireUhlrich <gregoire.uhlrich@cern.ch>
2022-04-30 21:52:32 +0200

    vector-0.1 + test_std.c

--- eco_perf/eco_std/vector.c
+++ eco_perf/eco_std/vector.c
@@ -2,7 +2,16 @@
 #include "error.h"
 #include "memory.h"
 
-void init_es_vector(es_vector_t *vector, es_size_t object_size)
+void _bound_check(es_size_t pos, es_size_t size)
+{
+    ES_ASSERT(
+        pos < size,
+        ES_INDEX_ERROR,
+        "Index %d is out of bounds of vector of size %d.",
+        pos, size)
+}
+
+void es_vector_init(es_vector_t *vector, es_size_t object_size)
 {
     ES_ASSERT(
         object_size > 0,
@@ -12,19 +21,6 @@ void init_es_vector(es_vector_t *vector, es_size_t object_size)
     vector->object_size = object_size;
     vector->size = 0;
     vector->_memory_size = 0;
-}
-
-void es_vector_push(es_vector_t *vector, es_object_t data)
-{
-    if (vector->size == vector->_memory_size)
-    {
-        vector->data = es_realloc(
-            vector->data,
-            2 * vector->_memory_size * vector->object_size);
-        vector->_memory_size *= 2;
-    }
-    vector->data[vector->size] = data;
-    ++vector->size;
 }
 
 void es_vector_reserve(es_vector_t *vector, es_size_t size)
@@ -38,13 +34,53 @@ void es_vector_reserve(es_vector_t *vector, es_size_t size)
     vector->_memory_size = size;
 }
 
-void clear_es_vector(es_vector_t *vector)
+void es_vector_clear(es_vector_t *vector)
 {
     vector->size = 0;
 }
 
-void free_es_vector(es_vector_t *vector)
+void es_vector_free(es_vector_t *vector)
 {
     es_free(vector->data);
-    init_es_vector(vector, vector->object_size);
+    es_vector_init(vector, vector->object_size);
+}
+
+void es_vector_push(es_vector_t *vector, void *value_ptr)
+{
+    if (vector->size == vector->_memory_size)
+    {
+        vector->data = es_realloc(
+            vector->data,
+            2 * vector->_memory_size * vector->object_size);
+        vector->_memory_size *= 2;
+    }
+    memcpy(
+        vector->data + vector->object_size * vector->size,
+        value_ptr,
+        vector->object_size);
+    ++vector->size;
+}
+
+void es_vector_erase(es_vector_t *vector, es_size_t pos)
+{
+    const es_size_t size = vector->size;
+    const es_size_t object_size = vector->object_size;
+    _bound_check(pos, size);
+    void *first = &vector[pos];
+    void const *last = first + (size - pos) * object_size;
+    while (first + object_size != last)
+    {
+        void *next = first + object_size;
+        memcpy(first, next, object_size);
+        first = next;
+    }
+    --vector->size;
+}
+
+void es_vector_summary(es_vector_t const *const vector)
+{
+    printf("vector[sizeof %u](size = %u, memory = %u]\n",
+           vector->object_size,
+           vector->size,
+           vector->_memory_size);
 }
--- eco_perf/eco_std/vector.h
+++ eco_perf/eco_std/vector.h
@@ -5,20 +5,24 @@
 
 typedef struct ESVector
 {
-    es_object_t *data;
+    void *data;
     es_size_t object_size;
     es_size_t size;
     es_size_t _memory_size;
 } es_vector_t;
 
-void init_es_vector(es_vector_t *vector, es_size_t object_size);
-
-void es_vector_push(es_vector_t *vector, es_object_t data);
+void es_vector_init(es_vector_t *vector, es_size_t object_size);
 
 void es_vector_reserve(es_vector_t *vector, es_size_t size);
 
-void clear_es_vector(es_vector_t *vector);
+void es_vector_clear(es_vector_t *vector);
 
-void free_es_vector(es_vector_t *vector);
+void es_vector_free(es_vector_t *vector);
+
+void es_vector_push(es_vector_t *vector, void *value_ptr);
+
+void es_vector_erase(es_vector_t *vector, es_size_t pos);
+
+void es_vector_summary(es_vector_t const *const vector);
 
 #endif
